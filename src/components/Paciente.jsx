3bc3e494aa95518d1b1b114644b3b6f8eda49d4c
Hugo Oyola <hugo212h@gmail.com>
2023-09-27 10:10:24 -0500

    ✨ Interfaz creada

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,31 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}
+        <span className="font-normal normal-case">Hugo</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {""}
+        <span className="font-normal normal-case">hugo212h@gmail.com</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha de Alta: {""}
+        <span className="font-normal normal-case">27 de set. 2023</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Síntomas: {""}
+        <span className="font-normal normal-case">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, eum? Sequi id iusto mollitia aspernatur in, earum, deleniti ab officia dignissimos, nisi suscipit
+          laudantium ad voluptatum harum exercitationem quaerat labore!
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Paciente;
